5329afbef65799531cfbe9e835c60ec8d59805d3
Adam Joines <amountcastlej@gmail.com>
2023-06-14 22:57:52 -0400

    first proj

---
+++ FirstProgram.java
@@ -0,0 +1,8 @@
+
+public class FirstProgram {
+    public static void main(String[] args){
+        System.out.println("My name is Adam Joines");
+        System.out.println("I am 31 years old");
+        System.out.println("My hometown is Chapel Hill, NC");
+    }
+}
